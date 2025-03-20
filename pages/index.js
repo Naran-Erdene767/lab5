a6157e878484efa4bae5ce71aa8cee93a056a0b6
Naran-Erdene <narkasuldee@gmail.com>
2025-03-20 14:33:06 +0800

    ?A?S

--- pages/index.js
+++ pages/index.js
@@ -1,113 +1,98 @@
-import Image from "next/image";
-import { Geist, Geist_Mono } from "next/font/google";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+const exampleData = [
+  {
+    id: 1,
+    firstn: "Naran-Erdene",
+    lastn: "Gankhuyag",
+    school: "Nest",
+    job: "student",
+    status: [
+      { id: 10, name: "water bottle" },
+      { id: 11, name: "windows" }
+    ],
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSvYjxDXSt0uLzdMS91Z7jP-9KPiFfszx6uHw&s"
+  },
+  {
+    id: 2,
+    firstn: "Aidaulet",
+    lastn: "Nurbolsin",
+    school: "Nest",
+    job: "student",
+    status: [
+      { id: 10, name: "eyeglass" },
+      { id: 11, name: "windows" }
+    ],
+    image:
+      "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/68/01/e9/6801e9f9-a7aa-177c-43fa-0cb9594a5d36/AppIcon-0-0-1x_U007emarketing-0-7-0-85-220.png/256x256bb.jpg"
+  },
+  // Additional profiles...
+];
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+export default function lab5() {
+  const [filter, setFilter] = useState("");
+  const [grid, setGrid] = useState(false);
+  const router = useRouter();
+  const Click = () => {
+    setGrid(!grid);
+  };
 
-export default function Home() {
+  const filteredData = exampleData.filter((element) => {
+    return filter.toLocaleLowerCase() === "" 
+      ? element 
+      : element.firstn.toLocaleLowerCase().includes(filter.toLocaleLowerCase());
+  });
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
+    <div className="p-10 bg-gray-200 h-max">
+      <div className="mb-4">
+        <input
+          type="text"
+          value={filter}
+          onChange={(e) => setFilter(e.target.value)}
+          placeholder="Search by Firstname"
+          className="p-2 rounded-md border-2 border-gray-300 w-100 text-black"
         />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              pages/index.js
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </div>
+      <button 
+        className="h-12 w-96 bg-gradient-to-t from-sky-500 to-indigo-500 rounded-xl cursor-pointer mt-4 hover:scale-105 transition-all"
+        onClick={Click}>
+        Toggle Layout
+      </button>
+      <div className={grid ? "grid grid-cols-4 gap-8 mt-8" : "flex flex-col mt-8"}>
+        {filteredData.length === 0 ? (
+          <p className="text-black mt-4 text-xl">No matches found</p>
+        ) : (
+          filteredData.map((element) => (
+            <div
+              key={element.id}
+              className="w-full sm:w-96 h-auto flex flex-col p-5 rounded-lg text-black shadow-lg border-4 border-indigo-400 mt-8 hover:bg-gradient-to-r from-indigo-300 to-purple-300 hover:scale-105 transition-all"
+            >
+              <img
+                src={element.image}
+                alt={`${element.firstn} ${element.lastn}`}
+                className="object-cover rounded-full w-32 h-32 mx-auto mb-4 shadow-md"
+              />
+              <div className="text-black ml-2">
+                <p className="text-lg font-semibold">{element.firstn} {element.lastn}</p>
+              </div>
+              <p className="text-green-500 text-md ml-2">{element.school} - {element.job}</p>
+              {element.status && (
+                <div className="mt-2 ml-2">
+                  <p className="text-blue-400 font-bold">Status:</p>
+                  <ul className="text-black">
+                    {element.status.map((item) => (
+                      <p key={item.id} className="text-sm">{item.name}</p>
+                    ))}
+                  </ul>
+                </div>
+              )}
+            </div>
+          ))
+        )}
+      </div>
     </div>
   );
 }
